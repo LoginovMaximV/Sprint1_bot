1e86fa035ccfb5eb789d252762055198838dbd9a
Kostya Grigorev <uhbujhmtdrjcnz@gmail.com>
2024-03-06 22:19:29 +0500

    Interface_commit2: add functional buttons

--- main.py
+++ main.py
@@ -7,32 +7,50 @@ from aiogram import Bot, Dispatcher, F, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-#from aiogram.utils.markdown import hbold
-from keyboards import contact_keyboard
+import keyboards as kb
 from dotenv import load_dotenv
-from aiogram.exceptions import AiogramError
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher()
 admin_phone_number = os.getenv("ADMIN_NUMBER")
+user_contact = ''
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(text="Здравствуйте!\n"
-                              "Отправьте номер телефона для авторизации.", reply_markup=contact_keyboard())
+                              "Отправьте номер телефона для авторизации.", reply_markup=kb.contact_keyboard())
 
 @dp.message(F.contact)
 async def get_contact(message: types.Message):
     contact = message.contact
+    global user_contact
+    user_contact = str(contact.phone_number)
     if contact.phone_number == admin_phone_number:
-        await message.answer("Добро пожаловать!")
-        try:
-            await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id - 1)
-        except AiogramError:
-            pass
+        await message.answer("Добро пожаловать!", reply_markup=kb.function_keyboard())
+        user_contact = str(contact.phone_number)
     else:
         await message.answer("Данного номера нет в базе сотрудников.")
+@dp.message(F.text == 'Подать заявку')
+async def new_report(message: types.Message):
+    if user_contact == admin_phone_number:
+        await message.answer("Здесь будет реализована функция подача заявки.")
+    else:
+        await message.answer(f"Доступ к функции ограничен. {user_contact}")
+
+@dp.message(F.text == 'Просмотр заявок')
+async def new_report(message: types.Message):
+    if user_contact == admin_phone_number:
+        await message.answer("Здесь будет реализован функция просмотра заявок.")
+    else:
+        await message.answer(f"Доступ к функции ограничен. {user_contact}")
+
+@dp.message(F.text == 'Редактировать заявку')
+async def new_report(message: types.Message):
+    if user_contact == admin_phone_number:
+        await message.answer("Здесь будет реализована функция редактирования заявок.")
+    else:
+        await message.answer(f"Доступ к функции ограничен. {user_contact}")
 
 
 async def main() -> None:
