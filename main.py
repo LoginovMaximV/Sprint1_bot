069d567d0b2e31b64904dd5558e29f0ebcaa421f
Kostya Grigorev <uhbujhmtdrjcnz@gmail.com>
2024-03-16 23:07:02 +0500

    Interface_commit3: minor fixes

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 import keyboards as kb
 from dotenv import load_dotenv
+from aiogram.client.bot import DefaultBotProperties
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -18,6 +19,8 @@ bot = Bot(TOKEN)
 dp = Dispatcher()
 admin_phone_number = os.getenv("ADMIN_NUMBER")
 user_contact = ''
+key_admin = False
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(text="Здравствуйте!\n"
@@ -27,37 +30,33 @@ async def command_start_handler(message: Message) -> None:
 async def get_contact(message: types.Message):
     contact = message.contact
     global user_contact
-    user_contact = str(contact.phone_number)[1:]
+    global key_admin
+    user_contact = str(contact.phone_number)[:1]
     if db_01.user_exist(user_contact):
         await message.answer("Добро пожаловать!", reply_markup=kb.function_keyboard())
         user_contact = str(contact.phone_number)
+        key_admin = True
     else:
         await message.answer(f"Данного номера нет в базе сотрудников.")
 
-@dp.message(F.text == 'Подать заявку')
+@dp.message(lambda message: key_admin == True and F.text == 'Подать заявку')
 async def new_report(message: types.Message):
-    if user_contact == admin_phone_number:
-        await message.answer("Здесь будет реализована функция подача заявки.")
-    else:
-        await message.answer(f"Доступ к функции ограничен. {user_contact}")
+    await message.answer("Здесь будет реализована функция подача заявки.")
 
-@dp.message(F.text == 'Просмотр заявок')
-async def new_report(message: types.Message):
-    if user_contact == admin_phone_number:
-        await message.answer("Здесь будет реализован функция просмотра заявок.")
-    else:
-        await message.answer(f"Доступ к функции ограничен. {user_contact}")
 
-@dp.message(F.text == 'Редактировать заявку')
-async def new_report(message: types.Message):
-    if user_contact == admin_phone_number:
-        await message.answer("Здесь будет реализована функция редактирования заявок.")
-    else:
-        await message.answer(f"Доступ к функции ограничен. {user_contact}")
+@dp.message(lambda message: key_admin == True and F.text == 'Просмотр заявок')
+async def view_report(message: types.Message):
+    await message.answer("Здесь будет реализован функция просмотра заявок.")
+
+
+@dp.message(lambda message: key_admin == True and F.text == 'Редактировать заявку')
+async def edit_report(message: types.Message):
+    await message.answer("Здесь будет реализована функция редактирования заявок.")
+
 
 
 async def main() -> None:
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
 
 
